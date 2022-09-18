19f5c8363dfb0586fd4b1cba8cf6dbadcfe67819
bhatshakran <bhatshakran@gmail.com>
2022-09-18 12:57:06 +0530

    Fixed shoutouts img issue

--- components/gradBorder.tsx
+++ components/gradBorder.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from 'next-themes';
 import React from 'react';
 
 interface Props {
@@ -6,8 +7,20 @@ interface Props {
 }
 
 const GradBorder: React.FC<Props> = ({ children, size }) => {
+  const { theme, setTheme } = useTheme();
+
   return (
-    <div className={size === 'large' ? 'large grad_border' : 'grad_border'}>
+    <div
+      className={`${
+        size === 'large'
+          ? `grad_border large  ${
+              theme === 'dark' ? 'before:bg-black' : 'before:bg-white'
+            }`
+          : `grad_border  ${
+              theme === 'dark' ? 'before:bg-black' : 'before:bg-white'
+            }`
+      }`}
+    >
       <div className='img_container'>{children}</div>
     </div>
   );
